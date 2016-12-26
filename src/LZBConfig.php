0adaa44d64e61f5f3ff08dedde28ba2337e942cd
wangshaolin <wangshaolin@liangzibao.cn>
2016-12-26 11:30:38 +0800

    first commit

---
+++ src/LZBConfig.php
@@ -0,0 +1,41 @@
+<?php
+#########################################################################
+# File Name: src/LZBConfig.php
+# Desc: 
+# Author: Shaolin Wang
+# mail: shaon86@163.com
+# Created Time: Fri 23 Dec 2016 05:34:41 PM CST
+#########################################################################
+
+class LZBConfig{
+    static $appKey = "6823490131d9146da878b66119dcd313";
+    static $url = "http://openapi.wangshaolin.devel.liangzibao.cn/getway/do";
+
+    static $lzbPublicKey = "-----BEGIN PUBLIC KEY-----
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDATSY6n29/weYGpL80lGEm6km/
+zOkvJ3KTlIlwwsF7t2KUbqxcDPLYkmPbZBHPyDX9HRg2NB5rqT4Yiq4yZj2lAlho
+Za5ojjGbS0u9XAVsVm1+JPeQ9H5qUrVh7lWZZWukePv/mESInxhpFEC9fiDr3IvF
+4qdIenuj9eOlDGx7nwIDAQAB
+-----END PUBLIC KEY-----";
+    static $merPrivateKey = "-----BEGIN RSA PRIVATE KEY-----
+MIICXQIBAAKBgQD5esEhJNwqDZ7dgtGYwZp6oiuy+prg5CJv2uVLZgr/73hnYJO4
+y76+4yVGhZWFNMUb+yB/2HmwP5uja5gS+hpRdTgwXDt9nbv93nNbYSNlIxq4wqpF
+0SSI93RYu/7BP1rJ+7gLle6uQER0ufAPp7nI11YEhu977kE2S4CXJERd8wIDAQAB
+AoGBANQXEsb9khpCOBOy2kMWHCEzaxkeSbr0133mRtahqqpISEECbrNEqyWW76KT
+gDidUxDlnoxxWYqHmOYGW2OOsf1uhWOtNBZXg7hRfOYW3zFAhM0m629qniB1lB3u
+GQSEIcRoxo7bp7fT/d6jX9fGCbbbz23SqgR9aQPPJnitgJYpAkEA/02udzz2D6WH
+qHbMtGN3/ZJ2hzKUI39EnmKlmApR7B+w9P/IPNj7PqlSNiHbFqadgSpd9rgx0o0+
+RNPKVrSTdQJBAPopAVCsvnJNeLyxEjKhz7asR2qhb7cFZjjkw8b7zoUPb3PCain7
+DmmHPIQKzj7Py8fMhXUQ4qUUvA70HL+NBscCQQCSjRD9OW02BhsdRTS0x/R1BsIr
+UYlf5wqIsK/F2wazuG1n9fwHkbaHfcLHeB+/nN0E7EEMAyf2aUW5VKnto7KRAkBT
+vy6Fh48qTO1CnjTtZmZXTU2LUQhJSTFzkoJINzxoJ0a0hj1sWHP2uCN2QPfS51Xd
+SYbULhbyFQLwLqEBL4E5AkAOGNnaAu+ii/zOcD4oZnkMp8dlIXre1oerKyEDKxCw
+J9qvfhVTip/PEyOD7rZtm7MMrp/KQtm1UrvfBpoCom8v
+-----END RSA PRIVATE KEY-----";
+    static $merPublicKey = "-----BEGIN PUBLIC KEY-----
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQD5esEhJNwqDZ7dgtGYwZp6oiuy
++prg5CJv2uVLZgr/73hnYJO4y76+4yVGhZWFNMUb+yB/2HmwP5uja5gS+hpRdTgw
+XDt9nbv93nNbYSNlIxq4wqpF0SSI93RYu/7BP1rJ+7gLle6uQER0ufAPp7nI11YE
+hu977kE2S4CXJERd8wIDAQAB
+-----END PUBLIC KEY-----";
+}
